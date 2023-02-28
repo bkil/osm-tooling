6182a8d37e05906135a3553eb7038285b36f8aff
bkil <b.K.il.h.u+tigbuh@gmail.com>
2023-02-28 12:20:48 +0100

    scraper/icit.sh: add

---
+++ src/scraper/icit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+main() {
+  if [ -z "$DOMAIN" ]; then
+    echo "error: need to set DOMAIN"
+    exit 1
+  fi
+
+  curl -A- https://$DOMAIN/ingatlanok 2>/dev/null |
+  sed -nr "
+    s~^\s*<a target=\"_blank\"[^>]*>([^<>]+)</a>$~\1~
+    T end
+    s~&#34;~\"~g
+    p
+    :end
+    "
+}
+
+main "$@"
