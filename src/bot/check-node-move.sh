288ace2c3d4081ab30784f1cf73c79fe93b10a70
bkil <b.K.il.h.u+tigbuh@gmail.com>
2024-02-05 22:23:05 +0100

    bot/check-node-move.sh: user agent, escape wget newlines

--- src/bot/check-node-move.sh
+++ src/bot/check-node-move.sh
@@ -2,29 +2,31 @@
 . `dirname "$(readlink -f "$0")"`/check-node-move.inc.sh
 
 net_fetch_state() {
-  wget -U- -O- --timeout=15 \
+  wget -O- --timeout=15 \
+    -U "check-node-move.sh/0.1" \
     "https://download.openstreetmap.fr/replication/europe/minute/state.txt"
 #    "http://download.geofabrik.de/europe/hungary-updates/state.txt"
 }
 
 net_fetch_part() {
-  wget -U- -O- --timeout=30 \
+  wget -O- --timeout=30 \
+    -U "check-node-move.sh/0.1" \
     "https://download.openstreetmap.fr/replication/europe/minute/$1/$2/$3.osc.gz"
 #    "http://download.geofabrik.de/europe/hungary-updates/$1/$2/$3.osc.gz"
 }
 
 net_fetch_ids() {
   wget \
-    -U- \
+    -U "check-node-move.sh/0.1" \
     -O - \
     --timeout=60 \
-    --post-data 'data=%5Bout%3Acsv(%0A\
-%3A%3A%22id%22%2C+\
-%3A%3Alat%2C+\
-%3A%3Alon%2C+\
-place%2C+\
+    --post-data "data=%5Bout%3Acsv(%0A\
+%3A%3A%22id%22%2C\
+%3A%3Alat%2C\
+%3A%3Alon%2C\
+place%2C\
 name%3B%0A\
-false%3B+%22%3B%22)%5D%5Btimeout%3A25%5D%3B%0A\
+false%3B%22%3B%22)%5D%5Btimeout%3A25%5D%3B%0A\
 area(id%3A3600021335)-%3E.searchArea%3B%0A\
 node%5B%7E%22^place$%22%7E%22^(\
 country|\
@@ -34,8 +36,8 @@ borough|\
 town|\
 village|\
 hamlet\
-)%22%5D(area.searchArea)%3B%0A\
-out%3B' \
+)$%22%5D(area.searchArea)%3B%0A\
+out%3B" \
     'https://overpass-api.de/api/interpreter'
 }
 
